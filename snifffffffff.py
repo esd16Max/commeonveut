2c3ffcbaa943b7f074a9f46cf021e020fa642c01
esd16Max <jeansnake54@laposte.net>
2018-12-12 09:49:38 +0100

    modif sniffff

--- snifffffffff.py
+++ snifffffffff.py
@@ -1,11 +1,9 @@
 from scapy.all import *
 
-import re
-
 def callback(packet):
 	if packet[TCP].payload:
 		pkt = str(packet[TCP].payload)
-		if packet[IP].dport == 80 and "POST" in str(packet[TCP].payload):
+		if packet[IP].dport == 80 and "POST" == str(packet[TCP].payload)[:4]:
 			print "-" * 60
 			print("\n{} to {}:{}:\n".format(packet[IP].src, packet[IP].dst, packet[IP].dport))
 			print "-" * 60
